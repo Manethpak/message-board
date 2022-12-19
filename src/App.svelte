538642fcf95486243aadadbb5a8dfdad73f6e38f
Manethpak <manethpak00@gmail.com>
2022-12-19 16:41:32 +0700

    initial

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import Header from './components/Header.svelte';
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main class="bg-neutral-700 min-h-screen text-white">
+  <Header />
+
+  <Counter />
+
+  <img src={svelteLogo} alt="Svelte logo" />
+</main>
+
---
+++ src/components/Header.svelte
@@ -0,0 +1,5 @@
+<script></script>
+
+<header class="flex flex-col justify-center items-center py-14">
+  <h1 class="text-4xl font-bold">Welcome to Message Board 📮</h1>
+</header>
