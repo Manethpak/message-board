ff07b9d4e98d904e1670758fb882a2d86ffad465
Manethpak <manethpak00@gmail.com>
2022-12-25 20:56:56 +0700

    rename

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -8,7 +8,7 @@
   let showMenu = false;
 </script>
 
-<div class="hidden md:block">
+<nav class="hidden md:block">
   <div class="flex gap-10 justify-center">
     <a href="/" use:link class="text-xl hover:text-gray-300">Home</a>
     <a href="/" use:link class="text-xl hover:text-gray-300">About Us</a>
@@ -18,15 +18,15 @@
     <a href="/login" use:link class="text-xl hover:text-gray-300">Login</a>
     <a href="/register" use:link class="text-xl hover:text-gray-300">Register</a>
     {:else}
-    <p class="text-xl">Hi, {$userStore.displayName}!</p>
+    <a href="/profile" use:link class="text-xl hover:text-gray-300">Hi, {$userStore.displayName}!</a>
     <button on:click={() => signOut(auth)} class="text-xl hover:text-gray-300">Logout</button>
     {/if}
     
   </div>
   <div class="w-[90vw] border"/>
-</div>
+</nav>
 
-<div class="block md:hidden">
+<nav class="block md:hidden">
   <div class="w-[90vw] border"/>
   <div class="flex justify-center">
     <button class="text-xl hover:text-gray-300" on:click={() => showMenu = !showMenu}>Menu</button>
@@ -35,16 +35,15 @@
   {#if showMenu}
   <ul transition:slide|local class="bg-neutral-600 w-full flex flex-col gap-4 py-2 px-4 mt-2 rounded-md">
     <a href="/" use:link class=" hover:text-gray-300">Home</a>
-    <a href="/" use:link class=" hover:text-gray-300">About Us</a>
+    <a href="/about-us" use:link class=" hover:text-gray-300">About Us</a>
     
     {#if $userStore === null}
     <a href="/login" use:link class=" hover:text-gray-300">Login</a>
     <a href="/register" use:link class=" hover:text-gray-300">Register</a>
     {:else}
-    <p class="">Hi, {$userStore.displayName}!</p>
+    <a href="/profile" use:link class="hover:text-gray-300">Hi, {$userStore.displayName}!</a>
     <button on:click={() => signOut(auth)} class=" hover:text-gray-300">Logout</button>
     {/if}
-
   </ul>
   {/if}
-</div>
+</nav>
