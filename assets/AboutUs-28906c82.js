433e91261efe888da56cf061847bef6000ef5e02
Manethpak <manethpak00@gmail.com>
2022-12-31 02:25:40 +0700

    Updates

---
+++ assets/AboutUs-28906c82.js
@@ -0,0 +1,7 @@
+import{S as o,i as n,s as l,e as c,b as p,c as i,n as s,g as u}from"./index-65fea35c.js";function h(t){let e;return{c(){e=c("article"),e.innerHTML=`<h1 class="svelte-y7u3j1">About Message Board</h1> 
+  
+  <p class="svelte-y7u3j1">Message Board is a simple web app that allows users to post a board and comments to other people&#39;s board, anonymously. Message Board was created with the purpose Message Board was created with the purpose of learning and practicing <a href="https://svelte.dev/" target="_blank" rel="noopener noreferrer" class="svelte-y7u3j1">Svelte</a> and <a href="https://firebase.google.com/" target="_blank" rel="noopener noreferrer" class="svelte-y7u3j1">Firebase</a></p> 
+
+  <p class="svelte-y7u3j1">Message Board is open source and you can view the source code or contribute it at <a href="https://github.com/Manethpak/message-board" target="_blank" rel="noopener noreferrer" class="svelte-y7u3j1">Github</a>.</p> 
+
+  <p class="svelte-y7u3j1">Created and Maintain by <a href="https://www.github.com/Manethpak" rel="noreferrer" target="_blank" class="svelte-y7u3j1">Maneth Pak</a>.</p>`,p(e,"class","my-10 max-w-xl space-y-4")},m(a,r){i(a,e,r)},p:s,i:s,o:s,d(a){a&&u(e)}}}class b extends o{constructor(e){super(),n(this,e,null,h,l,{})}}export{b as default};
