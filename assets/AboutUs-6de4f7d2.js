9aff9a06dad4e17a56aaf970fb75244ebb43ad77
Manethpak <manethpak00@gmail.com>
2023-01-02 01:50:57 +0700

    Updates

--- assets/AboutUs-6de4f7d2.js
+++ assets/AboutUs-6de4f7d2.js
@@ -1,4 +1,4 @@
-import{S as o,i as n,s as l,e as c,b as p,c as i,n as s,g as u}from"./index-4c3ca823.js";function h(t){let e;return{c(){e=c("article"),e.innerHTML=`<h1 class="svelte-y7u3j1">About Message Board</h1> 
+import{S as o,i as n,s as l,e as c,b as p,c as i,n as s,g as u}from"./index-713c0709.js";function h(t){let e;return{c(){e=c("article"),e.innerHTML=`<h1 class="svelte-y7u3j1">About Message Board</h1> 
   
   <p class="svelte-y7u3j1">Message Board is a simple web app that allows users to post a board and comments to other people&#39;s board, anonymously. Message Board was created with the purpose Message Board was created with the purpose of learning and practicing <a href="https://svelte.dev/" target="_blank" rel="noopener noreferrer" class="svelte-y7u3j1">Svelte</a> and <a href="https://firebase.google.com/" target="_blank" rel="noopener noreferrer" class="svelte-y7u3j1">Firebase</a></p> 
 
